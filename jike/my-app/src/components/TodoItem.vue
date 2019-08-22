f4746eca9c0647cddfa8cc6b683a4e1c00a74e5e
aisansan <609657770@qq.com>
2019-08-22 22:27:30 +0800

    1

--- jike/my-app/src/components/TodoItem.vue
+++ jike/my-app/src/components/TodoItem.vue
@@ -2,6 +2,7 @@
   <li class="item">
     <input type="checkbox" v-model="checked">
     <slot name="item" v-bind="{checked}"></slot>
+    <button v-on:click="handleClick">delete</button>
   </li>
 </template>
 
@@ -10,7 +11,14 @@ export default {
   props: ['item'],
   data() {
     return {
+    
+      item1:this.item,
       checked: false
+    }
+  },
+  methods:{
+    handleClick(){
+      console.log(this.item1);
     }
   }
 }
